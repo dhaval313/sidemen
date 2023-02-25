4b80dcd7fd65b4f089a8338c92a7d1fb6096148c
Smitbhatt007 <cristianoronny234@gmail.com>
2023-02-25 10:50:47 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sidemen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
